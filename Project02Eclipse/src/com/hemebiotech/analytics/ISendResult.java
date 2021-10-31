2107ef170a306be1194257167ff2e3a3ca06fce6
Nugah <nugah.dev@gmail.com>
2021-10-31 16:28:49 +0100

    Final version

--- Project02Eclipse/src/com/hemebiotech/analytics/ISendResult.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISendResult.java
@@ -1,11 +1,12 @@
 package com.hemebiotech.analytics;
 
 import java.util.Map;
-
+/**
+ * Envoie les données extrait à partir d'une map dans un nouveau fichier
+ */
 public interface ISendResult {
 	/**
 	 * @param map parcourt une map pour en générer un fichier
 	 */
 	public void fileWriter(Map<String, Integer> map);
-
 }
